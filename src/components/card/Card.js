efb86961adfe50d2970605c699b8b933ffc80298
naveedalirehmani <naveedalirehmani321@gmail.com>
2021-09-16 18:34:48 +0500

    login signup login implemented

--- src/components/card/Card.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Main } from './styleCard';
-
-export const Card = ({ children }) => {
-	return <Main>{children}</Main>;
-};
--- src/components/input/CardInput.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Input } from './StyleInput';
-
-export const CardInput = ({ placeholder, onChange }) => {
-	return <Input placeholder={placeholder} onChange={onChange} />;
-};
